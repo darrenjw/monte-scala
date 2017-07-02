e7781d4e3b8884d0791f518c3f83455af242dc5b
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-07-02 12:29:37 +0100

    add foldLeft and scanLeft to the generic collection typeclass

--- src/main/scala/montescala/GenericColl.scala
+++ src/main/scala/montescala/GenericColl.scala
@@ -28,6 +28,9 @@ object TypeClasses {
     def scan[A](ca: C[A])(z: A)(f: (A, A) => A): C[A]
     def zip[A, B](ca: C[A])(cb: C[B]): C[(A, B)]
     def length[A](ca: C[A]): Int
+    // some fundamentally sequential methods
+    def foldLeft[A,B](ca: C[A])(z: B)(f: (B, A) => B): B
+    def scanLeft[A,B](ca: C[A])(z: B)(f: (B, A) => B): C[B]
   }
 
   // import GenericColl.ops._
@@ -35,24 +38,29 @@ object TypeClasses {
   // Instance for Vector
   implicit val vGC: GenericColl[Vector] = new GenericColl[Vector] {
     def map[A, B](ca: Vector[A])(f: A => B): Vector[B] = ca map f
+    def flatMap[A, B, D[B] <: GenTraversable[B]](ca: Vector[A])(f: A => D[B]): Vector[B] = ca flatMap f
     def reduce[A](ca: Vector[A])(f: (A, A) => A): A = ca reduce f
     def scan[A](ca: Vector[A])(z: A)(f: (A, A) => A): Vector[A] = ca.scan(z)(f)
-    def flatMap[A, B, D[B] <: GenTraversable[B]](ca: Vector[A])(f: A => D[B]): Vector[B] = ca flatMap f
     def zip[A, B](ca: Vector[A])(cb: Vector[B]): Vector[(A, B)] = ca zip cb
     def length[A](ca: Vector[A]) = ca.length
+    def foldLeft[A,B](ca: Vector[A])(z: B)(f: (B, A) => B): B = ca.foldLeft(z)(f)
+    def scanLeft[A,B](ca: Vector[A])(z: B)(f: (B, A) => B): Vector[B] = ca.scanLeft(z)(f)
   }
 
   // Instance for ParVector
   implicit val pvGC: GenericColl[ParVector] = new GenericColl[ParVector] {
     def map[A, B](ca: ParVector[A])(f: A => B): ParVector[B] = ca map f
+    def flatMap[A, B, D[B] <: GenTraversable[B]](ca: ParVector[A])(f: A => D[B]): ParVector[B] = ca flatMap f
     def reduce[A](ca: ParVector[A])(f: (A, A) => A): A = ca reduce f
     def scan[A](ca: ParVector[A])(z: A)(f: (A, A) => A): ParVector[A] = ca.scan(z)(f)
-    def flatMap[A, B, D[B] <: GenTraversable[B]](ca: ParVector[A])(f: A => D[B]): ParVector[B] = ca flatMap f
     def zip[A, B](ca: ParVector[A])(cb: ParVector[B]): ParVector[(A, B)] = ca zip cb
     def length[A](ca: ParVector[A]) = ca.length
+    def foldLeft[A,B](ca: ParVector[A])(z: B)(f: (B, A) => B): B = ca.foldLeft(z)(f)
+    def scanLeft[A,B](ca: ParVector[A])(z: B)(f: (B, A) => B): ParVector[B] = ca.scanLeft(z)(f)
   }
 
-  // TODO: Implementation for Spark RDDs - in another project
+  // TODO: Implementation for Spark RDDs - but do in another project - don't want spark dependency in core library...
+
 
 
   // Add a .thin method to Stream
